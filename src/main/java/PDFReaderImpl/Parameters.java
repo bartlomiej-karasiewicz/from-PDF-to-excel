3af372284263c3bdca5f29973509b44dda73144d
Jan Kowalski <jan.kowalski@example.com>
2019-11-25 21:24:38 +0100

    Added lombok library

--- src/main/java/PDFReaderImpl/Parameters.java
+++ src/main/java/PDFReaderImpl/Parameters.java
@@ -1,30 +1,14 @@
 package PDFReaderImpl;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
 public class Parameters {
 	private String lastLine;
 	private String beforeLastLine;
 	private String regex;
-	
-	public String getRegex() {
-		return regex;
-	}
-	public String getLastLine() {
-		return lastLine;
-	}
-	public String getBeforeLastLine() {
-		return beforeLastLine;
-	}
-	public Parameters() {
-		
-	}
-	public Parameters(String regex,String beforeLastLine, String lastLine ) {
-		super();
-		this.lastLine = lastLine;
-		this.beforeLastLine = beforeLastLine;
-		this.regex = regex;
-	}
-	@Override
-	public String toString() {
-		return "Parameters [lastLine=" + lastLine + ", beforeLastLine=" + beforeLastLine + ", regex=" + regex + "]";
-	}
-	
 }
